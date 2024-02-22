f9b850752d3922a4b4db7c3015488b72cd2f8fd9
Paulo Pereira <paulovvp@outlook.com>
2024-02-21 22:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/Frm_Principal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal : Form
+    {
+        public Frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_DemonstracaoKey_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey formDemonstracao = new Frm_DemonstracaoKey();
+            formDemonstracao.ShowDialog();
+        }
+
+        private void Btn_HelloWorld_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld formHelloWorld = new Frm_HelloWorld();
+            formHelloWorld.ShowDialog();
+        }
+
+        private void Btn_Mascara_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara formMascara = new Frm_Mascara();
+            formMascara.ShowDialog();
+        }
+
+        private void Btn_ValidaCPF_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF formValidaCPF = new Frm_ValidaCPF();
+            formValidaCPF.ShowDialog();
+        }
+
+        private void Btn_ValidaCPF2_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 formValidaCPF2 = new Frm_ValidaCPF2();
+            formValidaCPF2.ShowDialog();
+        }
+
+        private void Btn_ValidaSenha_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha Frm_ValidaSenha = new Frm_ValidaSenha();
+            Frm_ValidaSenha.ShowDialog();
+        }
+    }
+}
---
+++ CursoWindowsForms/Frm_ValidaCPF2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2 : Form
+    {
+        public Frm_ValidaCPF2()
+        {
+            InitializeComponent();
+            Msk_CPF.Focus();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string vConteudo;
+            vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".","").Replace("-","");
+            vConteudo = vConteudo.Trim();
+
+            if(vConteudo == "")
+            {
+                MessageBox.Show("Você deve digitar um CPF", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Msk_CPF.Focus();
+            }
+            else if (vConteudo.Length != 11)
+            {
+                MessageBox.Show("O CPF deve ter 11 dígitos", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Msk_CPF.Focus();
+            }
+            else
+            {
+                if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    bool validaCPF = false;
+                    validaCPF = Validação.ValidarCPF(Msk_CPF.Text);
+                    if (validaCPF == true)
+                    {
+                        MessageBox.Show("CPF VÁLIDO", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPF INVÁLIDO", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            } 
+        }
+    }
+}
